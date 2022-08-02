5b450ac9118edac3daf8561589261e587ebf68e7
Wei Liu <weiliu2000@outlook.com>
2022-08-02 10:42:10 +0800

    Change HomeViewModel

--- app/src/main/java/com/example/appproject/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/appproject/ui/home/HomeViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.appproject.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
 
-class HomeViewModel:ViewModel() {
+class ViewModel: ViewModel() {
+    var news = MutableLiveData<ArticleItem>()
+    fun getDatas():List<ArticleItem> {
+        var articleList:MutableList<ArticleItem> = Test.articleList.data.datas
+        return articleList
+    }
 }
